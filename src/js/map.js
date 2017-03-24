2f69bbd513a95333a97c9dc35d3e994ca44bd32d
Laurence Elsdon <git@elsdon.me>
2017-03-24 11:50:37 +0000

    Remove EOL semicolons

--- src/js/map.js
+++ src/js/map.js
@@ -20,7 +20,7 @@ var style = {
 
 var distances = {
   sea: {
-    horizontal: 20,
+    horizontal: 20
   }
 }
 
@@ -34,15 +34,15 @@ var hexagon = {
   draw: {},
   grid: [],
   width: function() {
-    return this.radius * 3;
+    return this.radius * 3
   },
   height: function() {
-    return this.radius * 2 * Math.sqrt(3)/2;
+    return this.radius * 2 * Math.sqrt(3)/2
   }
 }
 
 hexagon.draw.gridHorizontal = function(map, startPosition, radius,count) {
-  var curPos = startPosition;
+  var curPos = startPosition
   var column = []
 
   // Create Column
@@ -50,7 +50,7 @@ hexagon.draw.gridHorizontal = function(map, startPosition, radius,count) {
     column = column.concat(hexagon.draw.gridVertical(map, curPos, hexagon.radius, hexagon.vertical))
 
     // Update the current position to draw next hexagon
-    curPos = google.maps.geometry.spherical.computeOffset(curPos, hexagon.width(), 90);
+    curPos = google.maps.geometry.spherical.computeOffset(curPos, hexagon.width(), 90)
   }
   return column
 }
@@ -58,7 +58,7 @@ hexagon.draw.gridHorizontal = function(map, startPosition, radius,count) {
 // Draw hexagon grid from
 // http://stackoverflow.com/questions/11761738/how-can-i-make-a-google-maps-api-v3-hexagon-tiled-map-preferably-coordinate-bas
 hexagon.draw.gridVertical = function(map, startPosition, radius,count) {
-  var curPos = startPosition;
+  var curPos = startPosition
   var row = []
   var row_two = []
 
@@ -68,29 +68,29 @@ hexagon.draw.gridVertical = function(map, startPosition, radius,count) {
     row.push(hexagon.draw.polygon(map, curPos, radius))
 
     // Create a second hexagon above and 30 degrees across
-    var secPos = google.maps.geometry.spherical.computeOffset(curPos, hexagon.height(), 60);
+    var secPos = google.maps.geometry.spherical.computeOffset(curPos, hexagon.height(), 60)
     row_two.push(hexagon.draw.polygon(map, secPos, radius))
 
     // Update the current position to draw next hexagon
-    curPos = google.maps.geometry.spherical.computeOffset(curPos, hexagon.height(), 0);
+    curPos = google.maps.geometry.spherical.computeOffset(curPos, hexagon.height(), 0)
   }
   return [row, row_two]
 }
 
 hexagon.draw.polygon = function(map,position,radius){
-  var coordinates = [];
+  var coordinates = []
   // Add a coordinate at every 60 degree angle of a circle
   for(var angle = 30; angle < 360; angle += 60) {
-    coordinates.push(google.maps.geometry.spherical.computeOffset(position, radius, angle));
+    coordinates.push(google.maps.geometry.spherical.computeOffset(position, radius, angle))
   }
 
   // Construct the polygon between the coordinates
   var polygon = new google.maps.Polygon({
     paths: coordinates
-  });
+  })
   polygon.setOptions(style.default)
-  polygon.setMap(map);
-  // map.setCenter(position);
+  polygon.setMap(map)
+  // map.setCenter(position)
   return polygon
 }
 
@@ -101,18 +101,18 @@ IconOverlay.prototype = new google.maps.OverlayView()
 function IconOverlay(bounds, image, map, scale) {
 
   // Initialize all properties.
-  this.bounds_ = bounds;
-  this.image_ = image;
-  this.map_ = map;
+  this.bounds_ = bounds
+  this.image_ = image
+  this.map_ = map
   this.scale_ = scale || 1
 
   // Define a property to hold the image's div. We'll
   // actually create this div upon receipt of the onAdd()
   // method so we'll leave it null for now.
-  this.div_ = null;
+  this.div_ = null
 
   // Explicitly call setMap on this overlay.
-  this.setMap(map);
+  this.setMap(map)
 }
 
 /**
@@ -121,65 +121,65 @@ function IconOverlay(bounds, image, map, scale) {
  */
 IconOverlay.prototype.onAdd = function() {
 
-  var div = document.createElement('div');
-  div.style.borderStyle = 'none';
-  div.style.borderWidth = '0px';
-  div.style.position = 'absolute';
-  div.style.zIndex = 30;
+  var div = document.createElement('div')
+  div.style.borderStyle = 'none'
+  div.style.borderWidth = '0px'
+  div.style.position = 'absolute'
+  div.style.zIndex = 30
   div.style.background = 'url(' + this.image_ + ') center center no-repeat'
   div.style.backgroundSize = 'contain'
 
-  this.div_ = div;
+  this.div_ = div
 
   // Add the element to the "overlayLayer" pane.
-  var panes = this.getPanes();
-  panes.overlayLayer.appendChild(div);
-};
+  var panes = this.getPanes()
+  panes.overlayLayer.appendChild(div)
+}
 
 IconOverlay.prototype.draw = function() {
 
   // We use the south-west and north-east
   // coordinates of the overlay to peg it to the correct position and size.
   // To do this, we need to retrieve the projection from the overlay.
-  var overlayProjection = this.getProjection();
+  var overlayProjection = this.getProjection()
 
   // Retrieve the south-west and north-east coordinates of this overlay
   // in LatLngs and convert them to pixel coordinates.
   // We'll use these coordinates to resize the div.
-  var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-  var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
+  var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest())
+  var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast())
 
   // Resize the image's div to fit the indicated dimensions.
-  var div = this.div_;
+  var div = this.div_
 
   var width = (ne.x - sw.x) * this.scale_
   var height = (sw.y - ne.y) * this.scale_
 
 
-  div.style.left = sw.x + 'px';
-  div.style.top = ne.y + 'px';
-  div.style.width = width + 'px';
-  div.style.height = height + 'px';
+  div.style.left = sw.x + 'px'
+  div.style.top = ne.y + 'px'
+  div.style.width = width + 'px'
+  div.style.height = height + 'px'
 
   if(this.scale_ !== 1) {
-    div.style.left = sw.x - width / (2 * this.scale_) + 'px';
-    div.style.top = ne.y - height / (2 * this.scale_) + 'px';
+    div.style.left = sw.x - width / (2 * this.scale_) + 'px'
+    div.style.top = ne.y - height / (2 * this.scale_) + 'px'
   }
 
-};
+}
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min)) + min
 }
 
 // The onRemove() method will be called automatically from the API if
 // we ever set the overlay's map property to 'null'.
 IconOverlay.prototype.onRemove = function() {
-  this.div_.parentNode.removeChild(this.div_);
-  this.div_ = null;
-};
+  this.div_.parentNode.removeChild(this.div_)
+  this.div_ = null
+}
 
 // Initilise Google Maps
 var initMap = function () {
@@ -187,11 +187,11 @@ var initMap = function () {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: origin
-  });
+  })
   var marker = new google.maps.Marker({
     position: origin,
     map: map
-  });
+  })
 
   // Extend Polygon class for find center method
   // http://stackoverflow.com/a/13772082/247218
@@ -217,7 +217,7 @@ var initMap = function () {
   // Shift start so origin is at centre
   start = google.maps.geometry.spherical.computeOffset(start, hexagon.height() * hexagon.horizontal / 2, 180)
   // Create our grid of hexagons
-  var grid = hexagon.draw.gridHorizontal(map, start, hexagon.radius, hexagon.horizontal);
+  var grid = hexagon.draw.gridHorizontal(map, start, hexagon.radius, hexagon.horizontal)
 
   // Hexagon mouseover events
   _.each(grid, function(column, x) {
